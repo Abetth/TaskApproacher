a6d1ecf7b1737dd65550f12abafe929d0ed8c643
Abetth <abetthmail@gmail.com>
2025-07-24 22:54:36 +0300

    Initial commit Task that is Approaching

---
+++ src/test/java/com/taskapproacher/TaskApproacher/TaskApproacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskapproacher.TaskApproacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApproacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
